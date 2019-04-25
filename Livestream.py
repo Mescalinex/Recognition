041c4e116c4c29c280f148d1c6361bf58f516d1c
Mikhail <mikhailmashchenko1@gmail.com>
2019-04-25 18:42:47 +0300

    second

--- Livestream.py
+++ Livestream.py
@@ -6,14 +6,15 @@ execution_path = os.getcwd()
 
 camera = cv.VideoCapture("http://195.189.181.205/mjpg/video.mjpg") 
 
+#camera = cv.VideoCapture("http://207.192.232.2:8000/mjpg/video.mjpg")
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
-detector.loadModel()
+detector.loadModel(detection_speed="faster")
 
 custom_objects = detector.CustomObjects(car=True, truck=True)
 
 video_path = detector.detectCustomObjectsFromVideo(camera_input=camera,
-                                output_file_path=os.path.join(execution_path, "camera_detected_1")
-                                , frames_per_second=20, log_progress=True)
+                                output_file_path=os.path.join(execution_path, "rtmp://a.rtmp.youtube.com/live2")
+                                , frames_per_second=30, log_progress=True)
 print(video_path)
